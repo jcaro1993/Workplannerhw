5e1cfe3d615f999f523f9e27604a17272b18ef01
Javier Caro <jcaro1993@gmail.com>
2020-11-07 18:51:20 -0800

    adding time

---
+++ script.js
@@ -0,0 +1,36 @@
+var currentDay = new Date($.now());
+$("#currentDay").html(currentDay);
+var dayContent = JSON.parse(localStorage.getItem("input"));
+
+if (dayContent === null) {
+    dayContent = ["", "", "", "", "", "", "", "", "", "", "", ""];
+}
+
+var now = moment();
+
+$('time').each(function(i, e) {
+    var currentDay = moment($(e).attr('datetime'));
+
+    if (now.diff(time, 'days') <= 1) {
+        $(e).html('<span>' + time.from(now) + '</span>');
+    }
+});
+
+
+for (i = 9; i < 17; i++) {
+    $("#" + i.toString()).html(dayContent[i - 9])
+    if (i === currentDay.getHours()) {
+        $("#" + i.toString()).attr("class", "form-control present");
+    }
+    if (i < currentDay.getHours()) {
+        $("#" + i.toString()).attr("class", "form-control past");
+    }
+    if (i > currentDay.getHours()) {
+        $("#" + i.toString()).attr("class", "form-control future");
+    }
+}
+$(".saveBtn").on("click", function(event) {
+    var input = $("#" + event.target.value).val();
+    dayContent.splice((event.target.value - 9), 1, input)
+    localStorage.setItem("input", JSON.stringify(dayContent))
+})
